e97ba6f85a15cab0592c03ed7d04408c0bba3e1a
Daniel Kim <danielkim107@gmail.com>
2021-01-01 13:24:02 +0900

    add relations

--- src/sequelize/apply-relation.js
+++ src/sequelize/apply-relation.js
@@ -1,6 +1,11 @@
 function applyRelation(sequelize) {
-	const { entry, user } = sequelize.models;
-	entry.belongsTo(user, {foreignKey: 'userId'});
+	const { entry, user, slot } = sequelize.models;
+	// Entry Relations
+	entry.belongsTo(user, { foreignKey: 'userId' });
+
+	// Slot Relations
+	slot.belongsTo(user, { foreignKey: 'adminId' });
+	slot.belongsTo(user, { foreignKey: 'studentId' });
 }
 
 module.exports = { applyRelation };
