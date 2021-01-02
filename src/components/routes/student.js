e761cbbab9b3842130cda8014cec66777a7efea4
Daniel Kim <danielkim107@gmail.com>
2021-01-02 13:09:14 +0900

    update api for student

--- src/components/routes/student.js
+++ src/components/routes/student.js
@@ -1,18 +1,48 @@
 import { sequelize } from '../../sequelize';
 
-const User = sequelize.models.user;
+const Student = sequelize.models.student;
 
 async function getList(req, res) {
 	const students = await User.findAll({
-		where: { isSuperuser: false, isAdmin: false, isStudent: true, },
-		attributes: ['id', 'username'],
+		attributes: ['id', 'name', 'defaultHour', 'price'],
 		order: [
-			['id', 'DESC'],
+			['name', 'DESC'],
 		],
 	});
 	res.status(200).send(students);
 };
 
+async function getById(req, res) {
+	const id = parseInt(req.params.id);
+	const student = await Student.findByPk(id);
+	if (student) {
+		res.status(200).send(student);
+	} else {
+		res.status(400).send({ message: 'Student not found. '});
+	}
+};
+
+async function create(req, res) {
+	await Student.create(req.body);
+	res.status(201).end();
+};
+
+async function update(req, res) {
+	const id = parseInt(req.params.id);
+	await Student.update(req.body, { where: { id: id } });
+	res.status(200).end();
+};
+
+async function remove(req, res) {
+	const id = parseInt(req.params.id);
+	await Student.destroy({ where: { id: id } });
+	res.status(204).end();
+};
+
 module.exports = {
 	getList,
-}
+	getById,
+	create,
+	update,
+	remove,
+};
