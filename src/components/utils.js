cf9ab72421c5e52128fbc0ffdb2d19392e58dd52
Daniel Kim <danielkim107@gmail.com>
2021-01-03 15:53:09 +0900

    remove uncessary relations and functions

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,15 +1,4 @@
-function getPaginationData(query) {
-	let data = {};
-	if (query) {
-		data.limit = query.limit;
-		data.page = query.page;
-	} else {
-		data.limit = 10;
-		data.page = 1;
-	}
-	data.offset = data.limit * (query.page - 1);
-	return data;
-};
+import { sequelize } from '../sequelize';
 
 function getParamId (params) {
 	return parseInt(params.id);
@@ -20,8 +9,31 @@ async function getModelCount(model) {
 	return data.count;
 };
 
+async function getDefaultStudentSlotInfo(teacherId, date) {
+	const today = new Date(date).getDay();
+	const Student = sequelize.models.student;
+	let studentHours = [];
+	let totalAmount = 0;
+	const students = await Student.findAll({
+		where: { teacherId: teacherId },
+		attributes: ['id', 'name', 'defaultHour', 'price', 'tutorDays',],
+		order: [
+			['name', 'DESC'],
+		],
+	});
+	students.forEach(student => {
+		if (student.tutorDays.includes(today)) {
+			studentHours.push({studentId: student.id, hours: student.defaultHour});
+			totalAmount += student.defaultHour * student.price;
+		} else {
+			studentHours.push({studentId: student.id, hours: 0});
+		}
+	});
+	return [studentHours, totalAmount];
+};
+
 module.exports = {
-	getPaginationData,
-	getModelCount,
 	getParamId,
+	getModelCount,
+	getDefaultStudentSlotInfo,
 };
--- src/sequelize/apply-relation.js
+++ src/sequelize/apply-relation.js
@@ -1,16 +1,13 @@
 function applyRelation(sequelize) {
-	sequelize.define('Student_Slots', {}, { timestamps: false });
 	const { student, teacher, slot } = sequelize.models;
 	// Student Relations
 	student.belongsTo(teacher, { foreignKey: 'teacherId', as: 'teacher' });
-	student.belongsToMany(slot, { through: 'Student_Slots'});
 
 	// Teacher Relations
 	teacher.hasMany(student, { foreignKey: 'teacherId', as: 'students' });
 
 	// Slot Relations
 	slot.belongsTo(teacher, { foreignKey: 'teacherId', as: 'teacher' });
-	slot.belongsToMany(student, { through: 'Student_Slots' });
 }
 
 module.exports = { applyRelation };
